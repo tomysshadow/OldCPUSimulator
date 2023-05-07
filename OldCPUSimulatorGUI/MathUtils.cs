07d65ec6fe4a5ac615e8da58af1dad2f32630713
DESKTOP-0Q3GGGJ\Anthony <ellescuba@gmail.com>
2023-05-07 00:10:37 -0600

    move MathUtils to its own file

---
+++ OldCPUSimulatorGUI/MathUtils.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OldCPUSimulatorGUI {
+    public static class MathUtils {
+        public static uint Clamp(uint number, uint min, uint max) {
+            return Math.Min(max, Math.Max(min, number));
+        }
+    }
+}
--- OldCPUSimulatorGUI/Program.cs
+++ OldCPUSimulatorGUI/Program.cs
@@ -4,14 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace OldCPUSimulatorGUI
-{
-	public static class MathUtils {
-		public static uint Clamp(uint number, uint min, uint max) {
-			return Math.Min(max, Math.Max(min, number));
-		}
-	}
-
+namespace OldCPUSimulatorGUI {
 	static class Program {
 		/// <summary>
 		/// The main entry point for the application.
